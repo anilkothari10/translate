c281ec4ebcf1f97445e9f384ad10b32fe80fe6a3
anilkothari10 <anilkothari@deloitte.com>
2018-09-05 23:34:21 +0530

    Updating table creation logic

--- src/main/java/translate/DocxFileConverter.java
+++ src/main/java/translate/DocxFileConverter.java
@@ -8,18 +8,95 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRow;
 
 public class DocxFileConverter {
 
 	public void docxFileConverter(File file, List<Attribute> attributeList, List<Rule> ruleList, Map<String, List<Util>> utilMap) throws Exception {
 		XWPFDocument docx = openDocxFile(file);
 		if (docx != null) {
+			List<XWPFTable> tables = docx.getTables();
+			for(XWPFTable table : tables){
+				XWPFTable tableFound = null;
+				List<XWPFTableRow> rows =  table.getRows();
+				
+				// Add Atrribute List
+				for(XWPFTableRow row : rows){
+					if(row.getCell(0).getText().equalsIgnoreCase("Attribute Name") 
+							&& row.getCell(1).getText().equalsIgnoreCase("Variable Name")
+							&& row.getCell(2).getText().equalsIgnoreCase("Data Type")){
+						tableFound = table;
+						break;
+					}
+				}
+				if(tableFound != null){
+					XWPFTableRow oldRow = tableFound.getRow(1);
+					for (Attribute attribute : attributeList) {
+						CTRow ctrow = CTRow.Factory.parse(oldRow.getCtRow().newInputStream());
+						XWPFTableRow newRow = new XWPFTableRow(ctrow, tableFound);
+						newRow.getCell(0).setText(attribute.getName());
+						newRow.getCell(1).setText(attribute.getVariableName());
+						newRow.getCell(2).setText(attribute.getDataType());
+						tableFound.addRow(newRow);
+					}
+					tableFound.removeRow(1);
+					tableFound = null;
+				}
+				
+				// Add Rule List
+				for(XWPFTableRow row : rows){
+					if(row.getCell(0).getText().equalsIgnoreCase("Rule Name") 
+							&& row.getCell(1).getText().equalsIgnoreCase("Variable Name")
+							&& row.getCell(2).getText().equalsIgnoreCase("Data Type")){
+						tableFound = table;
+					}
+				}
+				if(tableFound != null){
+					XWPFTableRow oldRow = tableFound.getRow(1);
+					for (Rule rule : ruleList) {
+						CTRow ctrow = CTRow.Factory.parse(oldRow.getCtRow().newInputStream());
+						XWPFTableRow newRow = new XWPFTableRow(ctrow, tableFound);
+						newRow.getCell(0).setText(rule.getName());
+						newRow.getCell(1).setText(rule.getVariableName());
+						newRow.getCell(2).setText(rule.getRuleType());
+						tableFound.addRow(newRow);
+					}
+					tableFound.removeRow(1);
+					tableFound = null;
+				}
+				
+				// Add Data Table List
+	            
+	            // Add Util Libraries map
+				for(XWPFTableRow row : rows){
+					if(row.getCell(0).getText().equalsIgnoreCase("Util Name") 
+							&& row.getCell(1).getText().equalsIgnoreCase("Variable Name")
+							&& row.getCell(2).getText().equalsIgnoreCase("Script Text")){
+						tableFound = table;
+						break;
+					}
+				}
+				if(tableFound != null){
+					XWPFTableRow oldRow = tableFound.getRow(1);
+					for(String utilName : utilMap.keySet()){
+			            for (Util util : utilMap.get(utilName)) {
+			            	CTRow ctrow = CTRow.Factory.parse(oldRow.getCtRow().newInputStream());
+							XWPFTableRow newRow = new XWPFTableRow(ctrow, tableFound);
+							newRow.getCell(0).setText(util.getName());
+							newRow.getCell(1).setText(util.getVariableName());
+							newRow.getCell(2).setText(util.getScriptText());
+							tableFound.addRow(newRow);
+			            }
+					}
+					tableFound.removeRow(1);
+					tableFound = null;
+					break;
+				}
+			}
 			
-			// Add Atrribute List
+			/*// Add Atrribute List
 			XWPFParagraph paragraph = docx.createParagraph();
 			XWPFRun run = paragraph.createRun();
 			run.setText("Atrribute Table",0);
@@ -73,56 +150,11 @@ public class DocxFileConverter {
 	                tempRow.getCell(1).setText(util.getVariableName());
 	                tempRow.getCell(2).setText(util.getScriptText());
 	            }
-			}
+			}*/
             
 			saveDocxFile(docx, Constants.SOURCE_FILE.replace("input", "output"));
 		}
 	}
-
-	/* Replaces table */
-	/*private long replaceTable(XWPFDocument doc) {
-		XWPFTable table = null;
-		long count = 0;
-		for (XWPFParagraph paragraph : doc.getParagraphs()) {
-			List<XWPFRun> runs = paragraph.getRuns();
-			String find = "%TABLE";
-			TextSegement found = paragraph.searchText(find, new PositionInParagraph());
-			if ( found != null ) {
-				count++;
-				if ( found.getBeginRun() == found.getEndRun() ) {
-					// whole search string is in one Run
-					XmlCursor cursor = paragraph.getCTP().newCursor();
-					table = doc.insertNewTbl(cursor);
-					XWPFRun run = runs.get(found.getBeginRun());
-					// Clear the "%TABLE" from doc
-					String runText = run.getText(run.getTextPosition());
-					String replaced = runText.replace(find, "");
-					run.setText(replaced, 0);
-				} else {
-					// The search string spans over more than one Run
-					StringBuilder b = new StringBuilder();
-					for (int runPos = found.getBeginRun(); runPos <= found.getEndRun(); runPos++) {
-						XWPFRun run = runs.get(runPos);
-						b.append(run.getText(run.getTextPosition()));
-					}                       
-					String connectedRuns = b.toString();
-					XmlCursor cursor = paragraph.getCTP().newCursor();
-					table = doc.insertNewTbl(cursor);
-					String replaced = connectedRuns.replace(find, ""); // Clear search text
-
-					// The first Run receives the replaced String of all connected Runs
-					XWPFRun partOne = runs.get(found.getBeginRun());
-					partOne.setText(replaced, 0);
-					// Removing the text in the other Runs.
-					for (int runPos = found.getBeginRun()+1; runPos <= found.getEndRun(); runPos++) {
-						XWPFRun partNext = runs.get(runPos);
-						partNext.setText("", 0);
-					}
-				}
-			}     
-		}
-		return count;
-	}*/
 
 	private XWPFDocument openDocxFile(File file) throws Exception {
 		XWPFDocument document = null;
