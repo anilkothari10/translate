d68744ab0106966bfe05abee2360e6a4585a6219
anilonline1370 <anilonline1370@gmail.com>
2018-11-04 17:55:47 +0530

    Update Constants.java

--- src/main/java/translate/Constants.java
+++ src/main/java/translate/Constants.java
@@ -22,15 +22,13 @@ public class Constants {
 	public static final String XML = "xml";
 	public static final String SECTIONTITLECOLOR = "800000";
 	public static final String SOURCE_COMMERCE_PROCESS_FILE = "files/input/Commerce/Process/";
-	//public static final String SOURCE_COMMERCE_DOCEDDOCUMENT_FILE = "files/input/Commerce/DocEdDocument new.xml";
 	public static final String SOURCE_COMMERCE_DOCEDDOCUMENT_FILE = "files/input/Commerce/DocEdDocument/";
 	public static final String CONFIGURATION_SECTION = "Configuration Section";
 	public static final String COMMERCE_SECTION = "Commerce Section";
+	public static final String WEB_SERVICES_DIR = "files/input/WebServices/";
 	public static final List<String> SECTION_TITILES = new ArrayList<String>(Arrays.asList(CONFIGURATION_SECTION, COMMERCE_SECTION));
 	public static final int SECTION_FONT_SIZE = 15;
 	public static final String DELOITTE_LOGO = "files/input/Deloitte_logo.png";
-	public static final String FILE_MANAGER_JS_DIR = "files/input/FileManager/JS/";
-	public static final String FILE_MANAGER_WEBSERVICES_DIR = "files/input/FileManager/WebServices/";
 	
 	private static Map<String, String> dataTypesMap = null;
 	
