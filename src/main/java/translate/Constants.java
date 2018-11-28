fd527695fbaebe437f8bda744be22fedccbc8297
anilkothari10 <anilkothari@deloitte.com>
2018-11-28 14:18:36 +0530

    Javascript changes

--- src/main/java/translate/Constants.java
+++ src/main/java/translate/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
 	public static final String TOC = "Table of Contents";
 	public static final String WEB_SERVICES_DIR = "files/input/WebServices/";
 	public static final String INTEGRATION_DIR = "files/input/Integration/";
-	public static final String JS_DIR = "files/input/JS/";
+	public static final String JS_DIR = "files/input/Javascript/";
 	public static final List<String> SECTION_TITILES = new ArrayList<String>(Arrays.asList(CONFIGURATION_SECTION, COMMERCE_SECTION,FILE_MANAGER_SECTION,TOC));
 	public static final int SECTION_FONT_SIZE = 15;
 	public static final String DELOITTE_LOGO = "files/input/Deloitte_logo.png";
